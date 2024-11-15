d7ea8e425a66237b1589b830369f54e39f7d0108
AleNaranjo-Git <aleunaranjo@gmail.com>
2024-11-14 21:16:30 -0600

    login y registrarse

---
+++ src/app/login/page.tsx
@@ -0,0 +1,116 @@
+"use client";
+
+import React, { useState } from "react";
+import { useRouter } from "next/navigation";
+
+const Form = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [rememberMe, setRememberMe] = useState(false);
+  const router = useRouter();
+
+  const handleSubmit = (e: { preventDefault: () => void }) => {
+    e.preventDefault();
+    console.log({ email, password, rememberMe });
+    router.push("/");
+  };
+
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-white">
+      <form
+        className="space-y-6 font-[sans-serif] text-[#333] max-w-md mx-auto"
+        onSubmit={handleSubmit}
+      >
+        <div className="relative flex items-center">
+          <input
+            type="email"
+            placeholder="Enter Email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            className="px-6 py-4 bg-[#f0f1f2] focus:bg-transparent w-full text-sm border outline-[#007bff] rounded transition-all"
+          />
+
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="#bbb"
+            stroke="#bbb"
+            className="w-[18px] h-[18px] absolute right-4"
+            viewBox="0 0 682.667 682.667"
+          >
+            <defs>
+              <clipPath id="a" clipPathUnits="userSpaceOnUse">
+                <path d="M0 512h512V0H0Z" data-original="#000000"></path>
+              </clipPath>
+            </defs>
+            <g clipPath="url(#a)" transform="matrix(1.33 0 0 -1.33 0 682.667)">
+              <path
+                fill="none"
+                strokeMiterlimit="10"
+                strokeWidth="40"
+                d="M452 444H60c-22.091 0-40-17.909-40-40v-39.446l212.127-157.782c14.17-10.54 33.576-10.54 47.746 0L492 364.554V404c0 22.091-17.909 40-40 40Z"
+                data-original="#000000"
+              ></path>
+              <path
+                d="M472 274.9V107.999c0-11.027-8.972-20-20-20H60c-11.028 0-20 8.973-20 20V274.9L0 304.652V107.999c0-33.084 26.916-60 60-60h392c33.084 0 60 26.916 60 60v196.653Z"
+                data-original="#000000"
+              ></path>
+            </g>
+          </svg>
+        </div>
+
+        <div className="relative flex items-center">
+          <input
+            type="password"
+            placeholder="Enter Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            className="px-6 py-4 bg-[#f0f1f2] focus:bg-transparent w-full text-sm border outline-[#007bff] rounded transition-all"
+          />
+
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="#bbb"
+            stroke="#bbb"
+            className="w-[18px] h-[18px] absolute right-4 cursor-pointer"
+            viewBox="0 0 128 128"
+          >
+            <path
+              d="M64 104C22.127 104 1.367 67.496.504 65.943a4 4 0 0 1 0-3.887C1.367 60.504 22.127 24 64 24s62.633 36.504 63.496 38.057a4 4 0 0 1 0 3.887C126.633 67.496 105.873 104 64 104zM8.707 63.994C13.465 71.205 32.146 96 64 96c31.955 0 50.553-24.775 55.293-31.994C114.535 56.795 95.854 32 64 32 32.045 32 13.447 56.775 8.707 63.994zM64 88c-13.234 0-24-10.766-24-24s10.766-24 24-24 24 10.766 24 24-10.766 24-24 24zm0-40c-8.822 0-16 7.178-16 16s7.178 16 16 16 16-7.178 16-16-7.178-16-16-16z"
+              data-original="#000000"
+            ></path>
+          </svg>
+        </div>
+
+        <div className="flex items-center">
+          <input
+            type="checkbox"
+            className="w-5 h-5 shrink-0"
+            checked={rememberMe}
+            onChange={(e) => setRememberMe(e.target.checked)}
+          />
+          <label className="text-sm ml-3">Recordarme</label>
+        </div>
+
+        <button
+          type="submit"
+          className="bg-white border border-black py-3 px-6 cursor-pointer font-bold w-full"
+        >
+          Registrarse
+        </button>
+
+        <hr className="my-6" />
+        <p className="text-center">
+          <a href="#" className="text-black hover:underline">
+            Olvidó la contraseña?
+          </a>{" "}
+          |{" "}
+          <a href="/registrarse" className="text-black hover:underline">
+            Crear cuenta
+          </a>
+        </p>
+      </form>
+    </div>
+  );
+};
+
+export default Form;
---
+++ src/app/registrarse/page.tsx
@@ -0,0 +1,89 @@
+"use client";
+
+import React, { useState } from "react";
+import { useRouter } from "next/navigation";
+
+const RegisterForm = () => {
+  const [email, setEmail] = useState("");
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const router = useRouter();
+
+  const handleSubmit = (e: { preventDefault: () => void }) => {
+    e.preventDefault();
+    console.log({ email, username, password, confirmPassword });
+
+    if (password === confirmPassword) {
+      // Redirige a la página home después de registrarse
+      router.push("/");
+    } else {
+      alert("Las contraseñas no coinciden");
+    }
+  };
+
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-white">
+      <form
+        className="space-y-6 font-[sans-serif] text-[#333] max-w-md mx-auto"
+        onSubmit={handleSubmit}
+      >
+        <div className="relative flex items-center">
+          <input
+            type="email"
+            placeholder="Correo Electrónico"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            className="px-6 py-4 bg-[#f0f1f2] focus:bg-transparent w-full text-sm border outline-[#007bff] rounded transition-all"
+          />
+        </div>
+
+        <div className="relative flex items-center">
+          <input
+            type="text"
+            placeholder="Nombre de Usuario"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+            className="px-6 py-4 bg-[#f0f1f2] focus:bg-transparent w-full text-sm border outline-[#007bff] rounded transition-all"
+          />
+        </div>
+
+        <div className="relative flex items-center">
+          <input
+            type="password"
+            placeholder="Contraseña"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            className="px-6 py-4 bg-[#f0f1f2] focus:bg-transparent w-full text-sm border outline-[#007bff] rounded transition-all"
+          />
+        </div>
+
+        <div className="relative flex items-center">
+          <input
+            type="password"
+            placeholder="Verificar Contraseña"
+            value={confirmPassword}
+            onChange={(e) => setConfirmPassword(e.target.value)}
+            className="px-6 py-4 bg-[#f0f1f2] focus:bg-transparent w-full text-sm border outline-[#007bff] rounded transition-all"
+          />
+        </div>
+
+        <button
+          type="submit"
+          className="bg-white border border-black py-3 px-6 cursor-pointer font-bold w-full"
+        >
+          Registrarse
+        </button>
+
+        <hr className="my-6" />
+        <p className="text-center">
+          <a href="/login" className="text-black hover:underline">
+            Inicio de sesión
+          </a>
+        </p>
+      </form>
+    </div>
+  );
+};
+
+export default RegisterForm;
